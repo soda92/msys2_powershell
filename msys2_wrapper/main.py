2a766dc31b90bc2fdbe3fcf252e70504fbad3964
sodacris <wjxa20152015@gmail.com>
2025-03-03 21:37:11 +0800

    fix environment

--- msys2_wrapper/main.py
+++ msys2_wrapper/main.py
@@ -2,6 +2,7 @@ from msys2_wrapper.find_msys2 import find_msys2
 import subprocess
 from msys2_wrapper.args import parse_args
 import sys
+import os
 
 
 def main():
@@ -12,6 +13,9 @@ def main():
         print(f"shell `{shell}` not exist", file=sys.stderr)
         exit(-1)
     env = {"CHERE_INVOKING": "1", "MSYSTEM": msystem}
+    for k in os.environ.keys():
+        if k not in env:
+            env[k] = os.environ.get(k, "")
 
     args2 = [str(shell_exe).replace("\\", "/"), *args]
     # print(" ".join(args2))
