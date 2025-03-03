58e657b4dbcbe1529ea37be63063336b423d0c41
sodacris <wjxa20152015@gmail.com>
2025-03-03 21:09:38 +0800

    add help

--- msys2_wrapper/args.py
+++ msys2_wrapper/args.py
@@ -1,8 +1,34 @@
 import sys
+from pathlib import Path
 
 
 def print_help():
-    pass
+    print(
+        (
+            """Usage:
+    %~1 [options] [login shell parameters]
+
+Options:
+    -mingw32 ^| -mingw64 ^| -ucrt64 ^| -clang64 ^|
+    -msys[2] ^| -clangarm64           Set shell type
+    -defterm ^| -mintty ^| -conemu     Set terminal type
+    -here                            Use current directory as working
+                                     directory
+    -where DIRECTORY                 Use specified DIRECTORY as working
+                                     directory
+    -[use-]full-path                 Use full current PATH variable
+                                     instead of trimming to minimal
+    -no-start                        Do not use "start" command and
+                                     return login shell resulting 
+                                     errorcode as this batch file 
+                                     resulting errorcode
+    -shell SHELL                     Set login shell
+    -help ^| --help ^| -? ^| /?         Display this help and exit
+
+Any parameter that cannot be treated as valid option and all
+following parameters are passed as login shell command parameters."""
+        ).replace("%~1", Path(sys.argv[0]).name)
+    )
 
 
 def parse_args() -> (str, str):
--- msys2_wrapper/main.py
+++ msys2_wrapper/main.py
@@ -2,9 +2,14 @@ from msys2_wrapper.find_msys2 import find_msys2
 import subprocess
 from msys2_wrapper.args import parse_args
 
-if __name__ == "__main__":
+
+def main():
     msystem, shell = parse_args()
     base = find_msys2()
     shell_exe = base / f"usr/bin/{shell}.exe"
-    env = {"CHERE_INVOKING": "1", "MSYSTEM": "UCRT64"}
+    env = {"CHERE_INVOKING": "1", "MSYSTEM": msystem}
     subprocess.call(shell_exe, env=env)
+
+
+if __name__ == "__main__":
+    main()
