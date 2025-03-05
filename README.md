5f2eec6096d6d55dbe81ad8ef45c503664dfd4fd
sodacris <wjxa20152015@gmail.com>
2025-03-05 08:53:30 +0800

    add doc, init config file support

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # msys2_wrapper
 
-a Python wrapper for starting msys2 without bat
+A Python wrapper for starting msys2 without bat
+
+## Prereq
+
+Has installed MSYS2 at disk root (e.g. `C:/msys64`), or `msys2.cmd` or `msys2_shell.cmd` can be found in PATH.
+
+also supports scoop-installed msys2.
+
+In case of multiple MSYS2 detected, it will launch the first found in `PATH`, and if not found, `C:/msys64` was checked, until `Z:/msys64`.
+
+You can also create a file `C:/Users/[your user name]/.config/msys2_wrapper.ini` to override the default, with the following contents:
+```ini
+[default]
+msys2_base = "D:/msys64"
+```
+
+## Usage
+
+A console launcher:
+
+```
+msys2c /?
+Usage:
+    msys2c [options] [login shell parameters]
+
+Options:
+    -mingw32 | -mingw64 | -ucrt64 | -clang64 |
+    -msys[2] | -clangarm64           Set shell type
+    -defterm | -mintty | -conemu     Set terminal type
+    -here                            Use current directory as working
+                                     directory
+    -where DIRECTORY                 Use specified DIRECTORY as working
+                                     directory
+    -[use-]full-path                 Use full current PATH variable
+                                     instead of trimming to minimal
+    -no-start                        Do not use "start" command and
+                                     return login shell resulting
+                                     errorcode as this batch file
+                                     resulting errorcode
+    -shell SHELL                     Set login shell
+    -help | --help | -? | /?         Display this help and exit
+
+Any parameter that cannot be treated as valid option and all
+following parameters are passed as login shell command parameters.
+```
+
+A GUI launcher `msys2w` was provided for launch with default `mintty` .
+
+## Source code
+
+<https://github.com/soda92/msys2_wrapper>
+
+## License
+
+APACHE
--- msys2_wrapper/find_msys2.py
+++ msys2_wrapper/find_msys2.py
@@ -10,10 +10,46 @@ def check_ext(s: Path) -> bool:
     return False
 
 
+# config_base, config_file, retcode
+def detect_config() -> (str, str, int):
+    config_file = Path.home().joinpath(".config").joinpath("msys2_wrapper.ini")
+    if not config_file.exists():
+        return "", "", -1
+    import configparser
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    if "default.msys2_base" not in config:
+        print(f"""incorrect config file {config_file}.
+please replace it with the following structure,
+with the "D:/msys64" replaced by your msys2 installation path:
+`
+[default]
+msys2_base = "D:/msys64"
+`
+""")
+        exit(-1)
+    p = config["default.msys2_base"]
+    return p, config_file, 0
+
+
 @functools.cache
 def find_msys2() -> Path:
+    config_base, config_file, retcode = detect_config()
+    if retcode == 0:
+        p = Path(config_base)
+        if not p.exists():
+            print(f"configed base {p} (in {config_file}) was not found")
+            exit(-1)
+        if not p.joinpath("msys2_shell.cmd").exists():
+            print(
+                f"configed base {p} (in {config_file}) was not an valid msys2 installation ({p.joinpath('msys2_shell.cmd')} was not found)"
+            )
+        return p
+
     p = os.environ["PATH"]
-    results = []
+    results: list[Path] = []
     for d in p.split(";"):
         pd = Path(d)
         if not (pd.exists() and pd.is_dir()):
@@ -24,12 +60,25 @@ def find_msys2() -> Path:
                 continue
             if pi.stem == "msys2":
                 results.append(pi)
+
+    if len(results) == 0:
+        for i in [chr(i + ord("C")) for i in range(26 - 2)]:
+            p = f"{i}:/msys64"
+            p2 = Path(p).joinpath("msys2_shell.cmd")
+            if p2.exists():
+                results.append(p2)
+                break
+
     results = list(filter(check_ext, results))
     if len(results) == 0:
         logging.critical("msys2 not found")
         exit(-1)
-    p1 = results[0]
-    real_loc = p1.parent
+
+    p1 = results[0].parent
+
+    # check scoop
     if "scoop/shims" in str(p1).replace("\\", "/"):
         real_loc = Path.home() / "scoop/apps/msys2/current/msys2_shell.cmd"
-    return real_loc.parent
+        p1 = real_loc.parent
+
+    return p1
